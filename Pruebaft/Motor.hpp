cce7ba94159d8a0e772015b21aec4f3258655279
tiziano-torterolo <tiziano.torterolo@estudiantes.utec.edu.uy>
2026-06-26 06:59:55 -0300

    b
    b

--- Pruebaft/Motor.hpp
+++ Pruebaft/Motor.hpp
@@ -1,34 +1,39 @@
 #pragma once
 #include <iostream>
 #include <memory>
-#include <concepts>
 #include <deque>
 #include <utility>
+#include <vector>
 #include <cmath>
 #include <algorithm>
 #include "PID.hpp"
+#define EXIST_VEHICLE true
 namespace Proyecto{
 template <typename Encoder, typename Counter>
 class Motor {
 private:
     std::shared_ptr<Encoder> encoder;
     std::shared_ptr<Counter> counter;
-    PID pid{69, 0.019, 1.95}; 
+    PID pid{0.6, 0.00136, 21.25}; 
     double speed = 0.0;
     double SP_speed = 170;
     std::deque<std::pair<uint64_t, int>> muestras;
 //    auto lastUpdateTime = std::chrono::steady_clock::now();
 //    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
     int lastCounterProbeRead = 0;
+    bool positiveDirection = false;
 
 public:
-    Motor(std::shared_ptr<Encoder> enc = nullptr, std::shared_ptr<Counter> cnt = nullptr) : encoder(enc), counter(cnt){    }
+    //std::vector<std::pair<uint64_t, double>> muestrassyn;
+    Motor(std::shared_ptr<Encoder> enc = nullptr, std::shared_ptr<Counter> cnt = nullptr) : encoder(enc), counter(cnt){   
+        //muestrassyn.reserve(5000);
+    }
 
     inline double getSpeed() const noexcept {
         return speed;
     }
     inline int getPower() const noexcept{
-        if constexpr (requires { encoder->getSpeed(); }) {
+        if constexpr (EXIST_VEHICLE) {
             if (!encoder) return 0;
             return static_cast<int>(encoder->getSpeed());
         }
@@ -40,14 +45,15 @@ public:
     }
 
     inline void setPower(int power) noexcept{
+        if constexpr (EXIST_VEHICLE) {
         if (!encoder) return;
-        if constexpr (requires { encoder->startSpeed(power); }) {
-            encoder->startSpeed(std::clamp(power, -520, 520));
+            encoder->startSpeed(std::clamp(power, -512, 512));
+            this->positiveDirection = (power>=0);
         }
     }
 
     inline int getDistance() const noexcept{
-        if constexpr (requires { counter->getDistance(); }) {
+        if constexpr (EXIST_VEHICLE) {
             if (!counter) return 0;
             return static_cast<int>(counter->getDistance());
         } 
@@ -69,6 +75,7 @@ public:
     
 
 private:
+
     void CounterProbe(auto timestamp) noexcept{
         auto distance = getDistance();
         if (muestras.size() > 4) {
@@ -78,9 +85,9 @@ private:
             muestras.emplace_back(timestamp, 10);
             return;
         }
-        muestras.emplace_back(timestamp, distance);
+
+        muestras.emplace_back(timestamp, distance==0 ? 0 : distance-lastCounterProbeRead);
         lastCounterProbeRead = distance;    
-        
     }
     void updateSpeedRead(double dt, auto timestamp) noexcept{
         CounterProbe(timestamp);
@@ -94,7 +101,7 @@ private:
         }
         double power = this->pid.calcular(SP_speed, speed, dt);
         setPower(static_cast<int>(std::round(power)));
-        //std::cout << "SP_speed: " << SP_speed << ", speed: " << speed << ", power: " << static_cast<int>(std::round(std::clamp(power, -520.0, 520.0))) << std::endl;
+        //std::cout << speed << std::endl;
 
     }
 
@@ -119,7 +126,9 @@ private:
                 
             
         }
-        speed = sum / cantMSE;
+        speed = this->positiveDirection ? sum / cantMSE : - (sum / cantMSE);
+        //muestrassyn.push_back({muestras.begin()->first,speed});
+
     }
 
 
--- Pruebaft/Prueba.cpp
+++ Pruebaft/Prueba.cpp
@@ -5,7 +5,8 @@
 #include <future>
 #include <ctime>
 #include <cstdint>
-//#include <ft/ft.hpp>
+#include <memory>
+#include <ft/ft.hpp>
 #include "Motor.hpp"
 
 using namespace std;
@@ -18,57 +19,75 @@ inline uint64_t now_ms();
 int main() {
     std::signal(SIGINT, signal_handler);
     std::cout << "Comenzando programa" << endl;
-    /*auto txt = ft::TXT{};
+    auto txt = ft::TXT{};
     std::cout << "txt = " << txt.getName() << endl;
-    auto* M1_Encoder = txt.getEncoder(1);
-    auto* M1_Counter = txt.getCounter(1);
+    auto M1_Encoder = std::shared_ptr<ft::Encoder>(txt.getEncoder(1));
+    auto M1_Counter = std::shared_ptr<ft::Counter>(txt.getCounter(1));
+    
+    auto M2_Encoder = std::shared_ptr<ft::Encoder>(txt.getEncoder(2));
+    auto M2_Counter = std::shared_ptr<ft::Counter>(txt.getCounter(2));
+    
+    auto M3_Encoder = std::shared_ptr<ft::Encoder>(txt.getEncoder(3));
+    auto M3_Counter = std::shared_ptr<ft::Counter>(txt.getCounter(3));
+    
+    auto M4_Encoder = std::shared_ptr<ft::Encoder>(txt.getEncoder(4));
+    auto M4_Counter = std::shared_ptr<ft::Counter>(txt.getCounter(4));
     txt.update_config();
-
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    
-    std::cout << "Iniciando Motores" << endl;
-
-    M1_Encoder->stop(0xFF);
-    M1_Encoder->startSpeed(200);
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    
-    M1_Encoder->startSpeed(500);
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     
 
-    M1_Encoder->startSpeed(-500);
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
-
-    M1_Encoder->stop(0xFF);
-    std::cout << "Finalizando programa" << endl;
-
-    delete M1_Encoder;
-    delete M1_Counter;
-*/
-
-    Proyecto::Motor<int,int> motor(nullptr, nullptr);
-    std::thread t( [&motor](){
+    Proyecto::Motor<ft::Encoder,ft::Counter> motor1(M1_Encoder, M1_Counter);
+    Proyecto::Motor<ft::Encoder,ft::Counter> motor2(M2_Encoder, M2_Counter);
+    Proyecto::Motor<ft::Encoder,ft::Counter> motor3(M3_Encoder, M3_Counter);
+    Proyecto::Motor<ft::Encoder,ft::Counter> motor4(M4_Encoder, M4_Counter);
+    
+    std::thread t( [&motor1,&motor2,&motor3,&motor4](){
         auto lastms = now_ms();
-        for (int i = 0; i < 115 && !stop; i++) {
+        for (int i = 0; i < 150 && !stop; i++) {
             auto now = now_ms();
             double dt = static_cast<double>(now - lastms);
             lastms = now;
-            (void)std::async(std::launch::async, [&motor, dt,now]() { motor.update(dt, now); });
-            std::this_thread::sleep_for(std::chrono::milliseconds(50));
+            motor1.update(dt, now);
+            motor2.update(dt, now);
+            motor3.update(dt, now);
+            motor4.update(dt, now);
+            std::this_thread::sleep_for(std::chrono::milliseconds(20));
         }
     });
     t.detach();
-    while (!stop) {
-        auto sp = 0.0;
-        std::cin >> sp;
-        motor.setSpeed(sp);
-    }
+    
+
+    motor1.setSpeed(100);
+    motor2.setSpeed(-100);
+    motor3.setSpeed(-100);
+    motor4.setSpeed(100);
+    std::this_thread::sleep_for(std::chrono::seconds(14));
 
     
-    std::cout << "prom =" << motor.getSpeed() << std::endl;
+    motor1.setSpeed(-100);
+    motor2.setSpeed(100);
+    motor3.setSpeed(100);
+    motor4.setSpeed(-100);
+    std::this_thread::sleep_for(std::chrono::seconds(14));
+
+    
+    motor1.setSpeed(100);
+    motor2.setSpeed(-100);
+    motor3.setSpeed(-100);
+    motor4.setSpeed(100);
+    std::this_thread::sleep_for(std::chrono::seconds(14));
+
+    
+    // 2. Traversal loop (using auto for cleaner code)
+    /*for (auto it = motor.muestrassyn.begin(); it != motor.muestrassyn.end(); ++it) {
+        std::cout << it->first << "," << it->second << std::endl; // Dereferencing to read data
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+*/
+    motor1.setPower(0);
+    motor2.setPower(0);
+    motor3.setPower(0);
+    motor4.setPower(0);
+
     std::cout << "Terminando programa" << endl;
 
 
